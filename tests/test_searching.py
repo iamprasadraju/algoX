30748da8b0ad97f1d5c9801a467f66e35fc32b63
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-12 23:34:58 +0530

    fixed tests

--- tests/test_searching.py
+++ tests/test_searching.py
@@ -1,15 +1,16 @@
 import unittest
-from dsax.search.BinarySearch import binarysearch
+from dsax.search import binarysearch, linearsearch
 from dsax.helpers import generate
 
 class TestSearching(unittest.TestCase):
 	def test_BinarySearch(self):
-		for _, arr in generate(upper_limit=1000, step=100):
-			tmp = sorted(arr)
-			target = tmp[-1]
-			self.assertEqual(binarysearch(tmp, target), len(arr) - 1)
-
-
+		for _, arr in generate(sort=True):
+			target = arr[-1]
+			self.assertEqual(binarysearch(arr, target), len(arr) - 1)
+	def test_LinearSearch(self):
+		for _, arr in generate(sort=True):
+			target = arr[-1]
+			self.assertEqual(linearsearch(arr, target), len(arr) - 1)
 
 if __name__ == "__main__":
 	unittest.main()
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,15 +1,12 @@
 import unittest
-from dsax.sort.SelectionSort import selection_sort
-from dsax.sort.BubbleSort import bubble_sort
+from dsax.sort import selectionsort
 from dsax.helpers import generate
 
 class TestSorting(unittest.TestCase):
 	def test_SelectionSort(self):
 		for _, arr in generate():
-			self.assertEqual(selection_sort(arr, reverse=True), sorted(arr, reverse=True))
-	def test_BubbleSort(self):
-		for _, arr in generate():
-			self.assertEqual(bubble_sort(arr), sorted(arr))
+			self.assertEqual(selectionsort(arr, reverse=True), sorted(arr, reverse=True))
+	
 
 if __name__ == "__main__":
 	unittest.main()
