d377ffcfc1b01cc8cb8d96f5eb59e8e7dc27ab5e
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-12 23:36:13 +0530

    micro fixes

--- perf/perf_SelectionSort.py
+++ perf/perf_SelectionSort.py
@@ -1,11 +1,11 @@
-from dsax.sort.SelectionSort import selection_sort
+from dsax.sort import selectionsort
 from dsax.helpers import animate, _timeit, generate
 import random
 
 @animate
 def perf_SelectionSort(step=50):
 	for size, arr in generate(upper_limit= 2000):
-		time = _timeit(selection_sort, arr)
+		time = _timeit(selectionsort, arr)
 		yield size, time
 
 perf_SelectionSort()
