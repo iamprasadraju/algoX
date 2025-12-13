c8e40d01abe58385a13bcff5139a4853cf0303b0
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-13 12:06:10 +0530

    imple insertion sort

--- dsax/sort.py
+++ dsax/sort.py
@@ -5,7 +5,7 @@ def selectionsort(arr: list[int], reverse: bool = False) -> list[int]:
 	if reverse:
 	 	comp = lambda a, b: (a < b)
 	else: 
-		comp = lambda a, b: (a >
+		comp = lambda a, b: (a > b)
 		
 	size = len(arr)
 	for i in range(size):
@@ -38,8 +38,26 @@ def bubblesort(arr: list[int], reverse: bool = False) -> list[int]:
 	return arr
 
 
+# Insertion Sort
+def insertionsort(arr: list[int], reverse: bool = False) -> list[int]:
+	if reverse:
+		comp = lambda a, b: (a < b)
+	else:
+		comp = lambda a, b: (a > b)
+		
+	size = len(arr)
+	for i in range(1, size):
+		key = arr[i]
+		j = i - 1
+		while j >=0 and comp(arr[j], key):
+			arr[j+1] = arr[j]
+			j -= 1
+		arr[j+1] = key
+	
+	return arr
+
 
 if __name__ == "__main__":
 	arr =  [random.randint(1, 10) for _ in range(5)]
 	print("Before sort: ", arr)
-	print("after sort: ", bubblesort(arr, reverse = True))
+	print("after sort: ", insertionsort(arr))
