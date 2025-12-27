0a0d9bd26afbf9db2485dd462aafee5697b15a30
iamprasadraju <iamprasadraju@duck.com>
2025-12-27 17:14:28 +0530

    imple Stack using Linked list

--- src/algox/ds/stack.py
+++ src/algox/ds/stack.py
@@ -19,8 +19,76 @@ class Stack:
 		return f"Stack({self._stack})"
 		
 	def __len__(self):
-		pass
+		return self.len
 		
 	def __iter__(self):
 		pass
 		
+
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+		
+class LinkedStack:
+	def __init__(self, arr=None):
+		self.len = 0
+		self.head = None
+		
+		if arr:
+			self.build(arr)
+			
+	def build(self, arr):
+		if not arr: return None
+		self.head = Node(arr[0])
+		self.len = 1
+		current = self.head
+		
+		for element in arr[1:]:
+			new_node = Node(element)
+			current.next = new_node
+			current = new_node
+			self.len += 1
+	
+	def pop(self):
+		if self.head is None:
+			return
+		
+		current = self.head
+		self.head = current.next
+		self.len -= 1
+		return current.data
+		
+			
+	def push(self, data):
+		if self.head is None:
+			return
+		new_node = Node(data)
+		new_node.next = self.head
+		self.head = new_node
+		self.len += 1
+		
+	def __repr__(self):
+		if self.head is None:
+			return "LinkedStack()"
+		
+		str = ""
+		current = self.head
+		while current:
+			str += f"[{current.data}]\n"
+			current = current.next
+		return "Stack(\n" + str + ")"
+		
+	def __iter__(self):
+		if self.head is None:
+			return None
+			
+		current = self.head
+		
+		while current:
+			data = current.data
+			current = current.next
+			yield data
+		
+	def __len__(self):
+		return self.len
