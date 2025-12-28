b8aaba1bbc7b50abc8d4e7c9dac978d9d772012b
iamprasadraju <iamprasadraju@duck.com>
2025-12-28 09:09:15 +0530

    rewrite tests for new version

--- tests/test_searching.py
+++ tests/test_searching.py
@@ -1,16 +1,16 @@
 import unittest
-from algox.search import binarysearch, linearsearch
+from algox.search import BinarySearch, LinearSearch
 from algox.helpers import generate
 
 class TestSearching(unittest.TestCase):
 	def test_BinarySearch(self):
 		for _, arr in generate(sort=True):
 			target = arr[-1]
-			self.assertEqual(binarysearch(arr, target), len(arr) - 1)
+			self.assertEqual(BinarySearch(arr, target), len(arr) - 1)
 	def test_LinearSearch(self):
 		for _, arr in generate(sort=True):
 			target = arr[-1]
-			self.assertEqual(linearsearch(arr, target), len(arr) - 1)
+			self.assertEqual(LinearSearch(arr, target), len(arr) - 1)
 
 if __name__ == "__main__":
 	unittest.main()
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,19 +1,19 @@
 import unittest
-from algox.sort import selectionsort, bubblesort, insertionsort
+from algox.sort import SelectionSort, BubbleSort, InsertionSort
 from algox.helpers import generate
 
 class TestSorting(unittest.TestCase):
 	def test_SelectionSort(self):
 		for _, arr in generate():
-			self.assertEqual(selectionsort(arr), sorted(arr))
+			self.assertEqual(SelectionSort(arr), sorted(arr))
 			
 	def test_BubbleSort(self):
 		for _, arr in generate():
-			self.assertEqual(bubblesort(arr), sorted(arr))
+			self.assertEqual(BubbleSort(arr), sorted(arr))
 			
 	def test_InsertionSort(self):
 		for _, arr in generate():
-			self.assertEqual(insertionsort(arr), sorted(arr))
+			self.assertEqual(InsertionSort(arr), sorted(arr))
 			
 	
 
